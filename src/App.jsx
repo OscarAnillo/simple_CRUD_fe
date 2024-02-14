afc892ed2e50a8330c6168d3cac18a4d9d8cfd28
Oscar Anillo <oscarani14@gmail.com>
2024-02-14 14:54:11 -0500

    add styles add title, author and addBookHandler fn

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,68 @@ function App() {
   const [author, setAuthor] = useState("");
   const [editingId, setEditingId] = useState(null);
 
+  const addTitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const addAuthorHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const addBookHandler = () => {
+    if (!title || !author) {
+      return alert("Please provide title and author of the book!");
+    }
+    setBooks([
+      ...books,
+      {
+        id: Date.now(),
+        title: title,
+        author: author,
+      },
+    ]);
+    setTitle("");
+    setAuthor("");
+  };
+
+  const deleteBookHandler = (id) => {
+    const filteredBooks = books.filter((b) => b.id !== id);
+    setBooks(filteredBooks);
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>Book Stall</h1>
-      <div>
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Author" />
-        {editingId ? <button>Update Book</button> : <button>Add Book</button>}
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={addTitleHandler}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={addAuthorHandler}
+        />
+        {editingId ? (
+          <button>Update Book</button>
+        ) : (
+          <button onClick={addBookHandler}>Add Book</button>
+        )}
       </div>
       <ul>
         {books.map((book) => (
           <li key={book.id}>
             {book.title} - {book.author}
-            <button>Edit</button>
-            <button>Delete</button>
+            <button className="edit">Edit</button>
+            <button
+              className="delete"
+              onClick={() => deleteBookHandler(book.id)}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
